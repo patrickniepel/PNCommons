94e89f8a2d0a8d061144800d9a04d3c74ba0f189
Patrick Niepel <patrickniepel@web.de>
2019-05-04 15:13:02 +0200

    • String+Utils

---
+++ PNCommons/PNCommons.h
@@ -0,0 +1,19 @@
+//
+//  PNCommons.h
+//  PNCommons
+//
+//  Created by Patrick Niepel on 04.05.19.
+//  Copyright © 2019 Patrick Niepel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PNCommons.
+FOUNDATION_EXPORT double PNCommonsVersionNumber;
+
+//! Project version string for PNCommons.
+FOUNDATION_EXPORT const unsigned char PNCommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PNCommons/PublicHeader.h>
+
+
